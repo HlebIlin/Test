aaab268e6826b07520529c43d63aa5fd0224a407
Hleb <illlingleb@gmail.com>
2022-06-28 15:55:35 +0300

    CheckHeaderZakaz

--- src/test/java/AutomationCourse/Ls13/CheckHeaderZakaz.java
+++ src/test/java/AutomationCourse/Ls13/CheckHeaderZakaz.java
@@ -1,2 +1,23 @@
-package AutomationCourse.Ls13;public class CheckHeaderZakaz {
+package AutomationCourse.Ls13;
+
+import TestInit.TestInit;
+import org.checkerframework.checker.units.qual.A;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CheckHeaderZakaz extends TestInit {
+    @Test
+    public void checkHeader(){
+        HeaderZakazHomePage headerZakazHomePage = new HeaderZakazHomePage(driver);
+        maximize();
+        headerZakazHomePage.goZakazOpen();
+
+        Assert.assertTrue(headerZakazHomePage.checkLogo().isDisplayed());
+        Assert.assertTrue(headerZakazHomePage.checkLanguage().isDisplayed());
+        Assert.assertTrue(headerZakazHomePage.checkPartners().isDisplayed());
+        Assert.assertTrue(headerZakazHomePage.checkChoiceShop().isDisplayed());
+
+
+
+    }
 }
--- src/test/java/AutomationCourse/Ls13/HeaderZakazHomePage.java
+++ src/test/java/AutomationCourse/Ls13/HeaderZakazHomePage.java
@@ -1,2 +1,27 @@
-package AutomationCourse.Ls13;public class HeaderZakazHomePage {
+package AutomationCourse.Ls13;
+
+import AutomationCourse.PageObject.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HeaderZakazHomePage extends BasePage {
+    public HeaderZakazHomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void goZakazOpen() {
+        driver.get("https://zakaz.ua/ru/");
+    }
+    public WebElement checkLogo(){
+        return getElementByXpath("//div[@class='jsx-3617752486 Header__leftNavigation']");
+    }
+    public WebElement checkLanguage(){
+        return getElementByXpath("//a[@href='/uk/']");
+    }
+    public WebElement checkPartners(){
+        return getElementByXpath("//div[@class='jsx-1112415212 DropDownContainer__header']");
+    }
+    public WebElement checkChoiceShop(){
+        return getElementByXpath("//button[text()='Выбрать магазин']");
+    }
 }
