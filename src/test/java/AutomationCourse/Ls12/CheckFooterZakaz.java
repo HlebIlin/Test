eb81ef38d8c349f89fd6c78ee05da96b447aecd5
Hleb <illlingleb@gmail.com>
2022-06-28 14:23:16 +0300

    clear

---
+++ src/test/java/AutomationCourse/Ls12/CheckFooterZakaz.java
@@ -0,0 +1,22 @@
+package AutomationCourse.Ls12;
+
+import TestInit.TestInit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CheckFooterZakaz extends TestInit {
+
+    @Test
+    public void checkZakaz() {
+        HomePageZakaz homePageZakaz = new HomePageZakaz(driver);
+        maximize();
+        homePageZakaz.goZakazOpen();
+
+        Assert.assertTrue(homePageZakaz.helpButton().isDisplayed());
+        Assert.assertTrue(homePageZakaz.facebookButton().isDisplayed());
+        Assert.assertTrue(homePageZakaz.appleButton().isDisplayed());
+        Assert.assertTrue(homePageZakaz.politicConfi().isDisplayed());
+
+
+    }
+}
