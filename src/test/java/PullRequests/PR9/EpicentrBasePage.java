be3fabe490bf68c3316adc258845027855da07e1
Hleb <illlingleb@gmail.com>
2022-07-01 13:11:11 +0300

    CheckHeaderEpicentr

--- src/test/java/PullRequests/PR9/EpicentrBasePage.java
+++ src/test/java/PullRequests/PR9/EpicentrBasePage.java
@@ -10,39 +10,51 @@ public class EpicentrBasePage extends BasePage {
     public EpicentrBasePage(WebDriver driver) {
         super(driver);
     }
+
     public void goEpicentr() {
         driver.get("https://epicentrk.ua/");
     }
+
     public WebElement getCatalogProductBtn() {
         return getElementByXpath("//div[@class='header__menu-opener-button-text']");
     }
+
     public WebElement getLogoBtn() {
         return getElementByXpath("//img[@alt='Епіцентр']");
     }
+
     public WebElement getTumblerLanguageBtn() {
         return getElementByXpath("//label[@class='switch__label']");
     }
+
     public WebElement getAddressFieldBtn() {
         return getElementByXpath("//div[@class='header__locations-street']");
     }
+
     public WebElement getSearch() {
         return getElementByXpath("//div[@data-is='Search']");
     }
+
     public WebElement getInformationBtn() {
         return getElementByXpath("//*[@class='icon header__info-toggle-info']");
     }
+
     public WebElement getVugodaBtn() {
         return getElementByXpath("//a[@title='Вигода']");
     }
+
     public List<WebElement> getCollationBtn() {
         return getElementsByXpath("//*[@class='icon icon-compare']");
     }
+
     public WebElement getFavoriteBtn() {
         return getElementByXpath("//*[@class='icon icon-favorite']");
     }
+
     public WebElement getSingInBtn() {
         return getElementByXpath("//div[@data-is='EnterBtn']");
     }
+
     public WebElement getCartBtn() {
         return getElementByXpath("//div[@data-is='SmallCart']");
     }
