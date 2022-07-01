03c52ca4e0b77203cd37d06f9134af3b526f6bac
Hleb <illlingleb@gmail.com>
2022-07-01 11:49:12 +0300

    CheckHeaderEpicentr

--- src/test/java/PullRequests/PR3/HomePagePayment.java
+++ src/test/java/PullRequests/PR3/HomePagePayment.java
@@ -1,6 +1,6 @@
 package PullRequests.PR3;
 
-import AutomationCourse.PageObject.BasePage;
+import PageObject.BasePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
